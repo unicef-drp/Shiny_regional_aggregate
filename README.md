4a86445ba4308cc50b6ef9ba4f5832bbaf86e208
liuyanguu <lyhello@gmail.com>
2019-10-16 13:27:11 -0400

    major revise, table format

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Shiny app to show regional aggregate
 
 ## D&A - Child Mortality Group 
-Initiated on Oct.7th, 2019.
+A prototype started on Oct.7th, 2019.
 Based on the new BWC aggragating method, the app simply wraps the original code.
 Based on the output of 2019.09 Child Mortality Report
 
-I suggest download the repo to open the app locally.
+I suggest download the repo to lunch the R project using the __`Rporj`__ file.
 ```
 git clone https://github.com/UnicefDAPM/Shiny_regional_aggregate.git
 ```
--- Shiny_Regional_Aggregate.R
+++ Shiny_Regional_Aggregate.R
@@ -15,6 +15,7 @@ library("DT")       # for shiny table
 library("ggplot2")
 library("data.table") 
 })
+options("digits" = 2)
 note1 <- "Note 1. This map is stylized and not to scale and does not reflect 
 a position by UNICEF on the legal status of any country or territory or the delimitation 
 of any country or territory or the delimitation of any frontiers."
@@ -48,32 +49,41 @@ ui = fluidPage(
   
   # main panel 
   mainPanel(
-    h3("Map of Selected Countries"),
-    leafletOutput(outputId = "mymap"),
-    p(note1),
-    h4("Country List:"),
-    textOutput(outputId = "selected_countries"),
-    br(), 
-    h3("Actions"),
-    actionButton("click_write",  "1. Confirm your selection", width = "50%"),
-    br(),
-    actionButton("click_run",    "2. Run aggregates", width = "50%"),
-    br(),
-    # plots:
-    h3("Results"),
-    textOutput(outputId = "selected_countries_on_file"),
-    plotOutput("plot_rate"),
-    plotOutput("plot_death"),
-    br(), 
-    # data:
-    h3("The most recent five years"),
-    DT::dataTableOutput("results_table_recent"),
-    h3("All the aggregated results"),
-    DT::dataTableOutput("results_table"),
-    br()
-    )
-)
-
+    # a 2-panel set up
+    tabsetPanel(
+      tabPanel("Run",
+        h3("Map of Selected Countries"),
+        leafletOutput(outputId = "mymap"),
+        p(note1),
+        h4("Country List:"),
+        textOutput(outputId = "selected_countries"),
+        br(), 
+        h3("Actions"),
+        p("Please confirm your selection first to double check the countries. Thank you."),
+        actionButton("click_write",  "1. Confirm your selection", width = "40%"),
+        actionButton("click_run",    "2. Run aggregates", width = "40%"),
+        br(),
+        # plots:
+        h3("Results"),
+        textOutput(outputId = "selected_countries_on_file"),
+        plotOutput("plot_rate"),
+        plotOutput("plot_death"),
+        br()
+      ),
+      tabPanel("Tables",
+               # data:
+               h3("The most recent five years"),
+               DT::dataTableOutput("results_table_recent", width = "80%"),
+               h3("All the aggregated results"),
+               DT::dataTableOutput("results_table", width = "80%"),
+               br()
+               ),
+      tabPanel("About",
+        p("Add some notes here.")       
+      )
+    ) #tabsetPanel
+  ) #mainpanel
+) #ui fluidpage
 
 # Server ------------------------------------------------------------------
 
@@ -93,14 +103,14 @@ server = function(input, output, session) {
   output$country_out_region <- renderUI({
     # select picker
     shinyWidgets::pickerInput(
-      inputId = "country_input_more", label = "",  
+      inputId = "country_input_more", label = "", 
       choices = countries, 
       selected = input$country_input,  # default select the countries in region
       multiple = TRUE,  # allow multiple selection
       options = list(
+        title = "Please select countries",
         `actions-box` = TRUE, 
-        size = 11,
-        `selected-text-format` = "count > 3"
+        size = 12
       )
     )
   })
@@ -140,15 +150,16 @@ server = function(input, output, session) {
   
   # check the names in `country.info.CME_adhoc.csv` file and 
   # run David's script `6outputaggregates.R`
-  get.results <- eventReactive(input$click_run, {
+  reactive.get.results <- eventReactive(input$click_run, {
     cnames <- data.table::fread("input/country.info.CME_adhoc.csv")[AdhocCountries=="Adhoc", sort(CountryName)]
     # only run if countries to be run >0 and equal selected countries
     if (length(cnames) >0 & identical(sort(cnames), sort(input$country_input_more))) {
-        # showModal will show that the scripe is running 
+        # showModal will show that the scripe is running, and removed when scripts are done 
         showModal(modalDialog("Running aggregate for", paste(cnames, collapse = ", "), footer=NULL))
           source(here::here("6outputaggregates.R"))
           message("Read the results file: 'Rates & Deaths_AdhocCountries.csv'.")
         removeModal()
+        if(!exists("file.dir.median")) file.dir.median <- "Aggregate results (median) 2019-08-15"
         return(fread(here::here(file.dir.median, "Rates & Deaths_AdhocCountries.csv")))
       } else {
         showModal(modalDialog(
@@ -158,15 +169,14 @@ server = function(input, output, session) {
         return(NULL)
       }
     
-    # output results 
   })
-
+  
   # Figure 1: Rate by year
   output$plot_rate <- renderPlot({
-    if (is.null(get.results())){
+    if (is.null(reactive.get.results())){
       return()
     }
-    dt_long <- data.table::melt(get.results(), measure.vars = c("U5MR median", "IMR median", "NMR median"), 
+    dt_long <- data.table::melt(reactive.get.results(), measure.vars = c("U5MR median", "IMR median", "NMR median"), 
                                 value.name = "rate", variable.name = "type_of_rate")
     ggplot(dt_long[!is.na(rate),], aes(x = Year, y = rate, color = Region, type = Region)) +
       geom_line(size = 1) + 
@@ -175,45 +185,57 @@ server = function(input, output, session) {
       labs(y = "Deaths per 1,000 live births") + 
       facet_wrap(facets= ~ type_of_rate) + 
       theme(legend.position="bottom") + 
-      scale_color_discrete(labels = c("Selected Countries", "World"))
+      scale_color_discrete(labels = c("Selected Countries", "World")) +
+      theme(plot.title = element_text(size = 20))
+    
     
   })
   
   # Figure 2: Number of deaths by year 
   output$plot_death <- renderPlot({
-    dt <- get.results()
+    dt <- reactive.get.results()
     if (is.null(dt)){
       return()
     }
     dt_long <- data.table::melt(dt, measure.vars = grep("deaths", colnames(dt), value = TRUE), 
                                 value.name = "deaths", variable.name = "type")
-    ggplot(dt_long[!is.na(deaths),], aes(x = Year, y = log10(deaths), color = Region)) +
+    ggplot(dt_long[!is.na(deaths),], aes(x = Year, y = deaths, color = Region)) +
       geom_line(size = 1) + 
       theme_bw() + 
       ggtitle("Median Deaths of U5MR, IMR, and NMR by Year") + 
-      labs(y = "log10(Deaths)") + 
+      labs(y = "Deaths") + 
       facet_wrap(facets= ~ type) + 
       theme(legend.position="bottom") + 
-      scale_color_discrete(labels = c("Selected Countries", "World"))
+      scale_color_discrete(labels = c("Selected Countries", "World")) + 
+      theme(plot.title = element_text(size = 20))
   })
   
   # print results as data table 
-  output$results_table_recent <-  DT::renderDataTable({
-    if (is.null(get.results())){
+  # a function to select columns and define some format 
+  get.table <- function(){
+    if (is.null(reactive.get.results())){
       return()
     }
-    # only print the most recent 5 years
-    get.results()[Region=="Adhoc"][order(-Year),][1:5,-1]
+    # remove some columns 
+    dt <- reactive.get.results()
+    dt <- dt[, -(grep("Population|population", colnames(dt), value = TRUE)), with = FALSE]
+    dt <- dt[Year>=1990][order(-Year)] # reorder
+    return(dt)
+    # DT::formatRound(datatable(dt), columns = grep("median", colnames(dt), value = TRUE), digits = 2)
+  }
+    
+  output$results_table_recent <-  DT::renderDT({
+    # only print the most recent 5 years of Adhoc group, order by year
+    DT::formatRound(
+      DT::datatable(get.table()[Region=="Adhoc"][1:5,-1]),
+      columns = c("U5MR median", "IMR median", "NMR median"), digits = 2)
   })
   
-  output$results_table <- DT::renderDataTable({
-    if (is.null(get.results())){
-      return()
-    } 
-    # prints all results
-    get.results()
+  output$results_table <- DT::renderDT({
+    DT::formatRound(
+      DT::datatable(get.table()), 
+      columns = c("U5MR median", "IMR median", "NMR median"), digits = 2)
   })
 }
-
 # Run App ---------------------------------------------------------------------
 shinyApp(ui, server)
