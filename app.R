4ec0bddf2250fc8a3a28dd31f2de5f1eaf1e7714
liuyanguu <lyhello@gmail.com>
2019-11-01 22:12:58 -0400

    [app] add gender, still need to fix some problem

--- app.R
+++ app.R
@@ -82,11 +82,19 @@ ui = fluidPage(
         actionButton("click_write",  "1. Confirm your selection", width = "40%"),
         actionButton("click_run",    "2. Run aggregates", width = "40%"),
         br(),
+        # to obtain gender results?
+        # after click_run, optional to run by gender
+    
+        # actionButton("click_run_gender", "3. Run aggregates by gender", width = "80%"),
+        # conditionalPanel("input.click_run",
+        #                 actionButton("click_run_gender", "3. Run aggregates by gender", width = "80%")),
+        br(),
         # plots:
-        h3("Results"),
+        h3("Plots"),
         textOutput(outputId = "selected_countries_on_file"),
-        plotOutput("plot_rate"),
-        plotOutput("plot_death"),
+        # plotOutput("plot_rate_gender", width = "800px", height = "800px"),
+        plotOutput("plot_rate",  width = "800px",  height = "400px"),
+        plotOutput("plot_death", width = "800px", height = "400px"),
         br()
       ),
       tabPanel("Tables",
@@ -100,6 +108,23 @@ ui = fluidPage(
                DT::dataTableOutput("results_table", width = "80%"),
                br()
                ),
+      
+      tabPanel("byGender",
+               # figures and tables
+               plotOutput("plot_rate_gender", width = "800px", height = "800px"),
+               br(),br(),
+               p(strong("Data for the female")),
+               downloadButton("download_table_f", "Download"),
+               br(),
+               DT::dataTableOutput("results_table_f", width = "80%"),
+               
+               br(),br(),
+               p(strong("Data for the male")),
+               downloadButton("download_table_m", "Download"),
+               br(),
+               DT::dataTableOutput("results_table_m", width = "80%"),
+               br()        
+      ),      
       # tabPanel ("About")
       tabPanel.about()
     ) #tabsetPanel
@@ -110,11 +135,16 @@ theme = "bootstrap.css"
 # Server ------------------------------------------------------------------
 
 server = function(input, output, session) {
-  # filter and return a vector of country names by selected region
-  reactive.get.sub.c = reactive({
-    # the countries selected, select by renderUI:country_input:
-    dc[dc$MDGRegion1%in%input$supplied_region, OfficialName]
-  })
+  # Render UI
+  # output$UI_tabPanel_byGender <- renderUI({
+  #   if (is.null(reactive.get.results.gender())){
+  #     return(NULL)
+  #   }
+  # 
+  #            
+  #   )
+  # })
+  
   # render check boxes of countries to send back to ui
   output$country_in_region <- renderUI({
     checkboxGroupInput(inputId = "country_input", label = "Countries",
@@ -137,6 +167,15 @@ server = function(input, output, session) {
     )
   })
   
+  
+  
+  
+  # filter and return a vector of country names by selected region
+  reactive.get.sub.c = reactive({
+    # the countries selected, select by renderUI:country_input:
+    dc[dc$MDGRegion1%in%input$supplied_region, OfficialName]
+  })
+
   # map the selected countries (if available in this map)
   output$mymap = renderLeaflet({
     validate(need(input$country_input_more, "Please select countries."))
@@ -161,6 +200,8 @@ server = function(input, output, session) {
     # removeModal()
   })
   
+  
+  # reactive.get.results ----------------------------------------------------
   # check the names in `country.info.CME_adhoc.csv` file and 
   # run David's script `6outputaggregates.R`
   reactive.get.results <- eventReactive(input$click_run, {
@@ -173,7 +214,7 @@ server = function(input, output, session) {
           source(here::here("6outputaggregates.R"))
           message("Read the results file: 'Rates & Deaths_AdhocCountries.csv'.")
         removeModal()
-        if(!exists("file.dir.median")) file.dir.median <- "Aggregate results (median) 2019-08-15"
+        file.dir.median <- "Aggregate results (median) 2019-08-15"
         return(fread(here::here(file.dir.median, "Rates & Deaths_AdhocCountries.csv")))
       } else {
         showModal(modalDialog(
@@ -185,14 +226,36 @@ server = function(input, output, session) {
     
   })
   
-  # Figure 1: Rate by year
-  output$plot_rate <- renderPlot({
-    if (is.null(reactive.get.results())){
-      return()
+  reactive.get.results.gender <- eventReactive(input$click_run, {
+    cnames <- data.table::fread("input/country.info.CME_adhoc.csv")[AdhocCountries=="Adhoc", sort(OfficialName)]
+    # only run if countries to be run >0 and equal selected countries
+    if (length(cnames) >0 & identical(sort(cnames), sort(input$country_input_more))) {
+      showModal(modalDialog("Running aggregate by gender for", paste(cnames, collapse = ", "), footer=NULL))
+      source(here::here("6outputaggregates_gender.R"))
+      removeModal()
+      return(
+        list(m = fread(here::here("Aggregate results (median) 2019-08-15 (male)", "Rates & Deaths_AdhocCountries.csv")),
+             f = fread(here::here("Aggregate results (median) 2019-08-15 (female)", "Rates & Deaths_AdhocCountries.csv")))
+        )
+    } else {
+      showModal(modalDialog(
+        "Please press <1. Confirm your selection> first.\n  The countries on the record file now are ", 
+        paste(cnames, collapse = ", "), ", which doesn't match your selected countries."
+      ))
+      return(NULL)
     }
-    dt_long <- data.table::melt(reactive.get.results(), measure.vars = c("U5MR median", "IMR median", "NMR median"), 
+    
+  })
+  
+
+  # Figure 1 Rate by Year ---------------------------------------------------
+  get.long <- function(dt, vars0){
+    dt_long <- data.table::melt(dt, measure.vars = vars0, 
                                 value.name = "rate", variable.name = "type_of_rate")
-    ggplot(dt_long[!is.na(rate),], aes(x = Year, y = rate, color = Region, type = Region)) +
+    return(dt_long[,.(Region, Year, rate, type_of_rate)])
+  }
+  plot.rate <- function(dt, vars0){
+    ggplot(get.long(dt, vars0)[!is.na(rate),], aes(x = Year, y = rate, color = Region, type = Region)) +
       geom_line(size = 1) + 
       theme_bw() + 
       ggtitle("Median U5MR, IMR, and NMR by Year") + 
@@ -201,38 +264,69 @@ server = function(input, output, session) {
       theme(legend.position = "bottom") + 
       scale_color_discrete(labels = c("Selected Countries", "World")) +
       theme(plot.title = element_text(size = 20))
-    
-    
+  }
+  
+  output$plot_rate <- renderPlot({
+    if (is.null(reactive.get.results())){
+      return()
+    }
+    vars0 <- c("U5MR median", "IMR median", "NMR median")
+    dt <- reactive.get.results()
+    plot.rate(dt, vars0)
   })
   
-  # Figure 2: Number of deaths by year 
+  output$plot_rate_gender <- renderPlot({
+    if (is.null(reactive.get.results.gender())){
+      return()
+    }
+    vars0 <- c("U5MR median", "IMR median")
+    dt_long_m <- get.long(reactive.get.results.gender()$m, vars0)
+    dt_long_f <- get.long(reactive.get.results.gender()$f, vars0)
+    dt_long_m$gender <- "Male"
+    dt_long_f$gender <- "Female"
+    dt_long <- rbind(dt_long_f, dt_long_m)
+    dt_long$Region <- as.factor(dt_long$Region)
+    levels(dt_long$Region) <- c("Selected Region", "World")
+    ggplot(dt_long[!is.na(rate),], aes(x = Year, y = rate, color = gender)) +
+      geom_line(size = 1) + 
+      theme_bw() + 
+      ggtitle("Median U5MR and IMR by Gender and Year", subtitle = "Selected Region vs. World") + 
+      labs(y = "Deaths per 1,000 live births", color = "Gender") + 
+      facet_wrap(facets = ~ type_of_rate + Region) + 
+      theme(legend.position = "bottom") + 
+      theme(plot.title = element_text(size = 20))
+  })
+  
+
+  # Figure 2. death by year -------------------------------------------------
+  plot.death <- function(dt){
+    dt_long <- data.table::melt(dt, measure.vars = grep("deaths", colnames(dt), value = TRUE), 
+                                  value.name = "deaths", variable.name = "type")
+    ggplot(dt_long[!is.na(deaths),], aes(x = Year, y = deaths, color = Region)) +
+        geom_line(size = 1) + 
+        theme_bw() + 
+        ggtitle("Median Deaths of U5MR, IMR, and NMR by Year") + 
+        labs(y = "Deaths") + 
+        facet_wrap(facets= ~ type) + 
+        theme(legend.position="bottom") + 
+        scale_color_discrete(labels = c("Selected Countries", "World")) + 
+        theme(plot.title = element_text(size = 20))
+  }
+  
   output$plot_death <- renderPlot({
     dt <- reactive.get.results()
     if (is.null(dt)){
       return()
     }
-    dt_long <- data.table::melt(dt, measure.vars = grep("deaths", colnames(dt), value = TRUE), 
-                                value.name = "deaths", variable.name = "type")
-    ggplot(dt_long[!is.na(deaths),], aes(x = Year, y = deaths, color = Region)) +
-      geom_line(size = 1) + 
-      theme_bw() + 
-      ggtitle("Median Deaths of U5MR, IMR, and NMR by Year") + 
-      labs(y = "Deaths") + 
-      facet_wrap(facets= ~ type) + 
-      theme(legend.position="bottom") + 
-      scale_color_discrete(labels = c("Selected Countries", "World")) + 
-      theme(plot.title = element_text(size = 20))
+    plot.death(dt)
   })
+  
   
 
   # print results as data table  --------------------------------------------
   # a function to select columns and define some format 
-  get.table <- function(){
-    if (is.null(reactive.get.results())){
-      return()
-    }
+  get.table <- function(dt){
     # remove some columns 
-    dt <- reactive.get.results()
     dt <- dt[, -(grep("Population|population", colnames(dt), value = TRUE)), with = FALSE]
     dt <- dt[Year>=1990][order( Region, -Year, )] # reorder
     return(dt)
@@ -240,29 +334,66 @@ server = function(input, output, session) {
   }
     
   output$results_table_recent <-  DT::renderDT({
+    if (is.null(reactive.get.results())){
+      return()
+    }
+    dt <- reactive.get.results()
+    
     # only print the most recent 5 years of Adhoc group, order by year
     DT::formatRound(
-      DT::datatable(get.table()[Region=="Adhoc"][1:5,-1]),
+      DT::datatable(get.table(dt)[Region=="Adhoc"][1:5,-1]),
       # format number columns with digits
       columns = c("U5MR median", "IMR median", "NMR median"), digits = 2)
   })
   
+
+  # results tables ----------------------------------------------------------
   output$results_table <- DT::renderDT({
+    if (is.null(reactive.get.results())){
+      return()
+    }
+    dt <- reactive.get.results()
+    
     DT::formatRound(
-      DT::datatable( get.table() ), 
+      DT::datatable( get.table(dt) ), 
       columns = c("U5MR median", "IMR median", "NMR median"), digits = 2)
   })
   
-  # Make results available for download
-  output$download_table <- downloadHandler(
-    filename <- function() {
-      paste0("Results ", Sys.Date(), ".csv")
-    },
-    content <- function(file) {
-      write.csv(get.table(), file, row.names = F, na = "")
+  output$results_table_m <- DT::renderDT({
+    if (is.null(reactive.get.results.gender())){
+      return()
     }
-  )
+    dt <- reactive.get.results.gender()$m
+    DT::formatRound(
+      DT::datatable( get.table(dt) ), 
+      columns = c("U5MR median", "IMR median"), digits = 2)
+  })
   
+  output$results_table_f <- DT::renderDT({
+    if (is.null(reactive.get.results.gender())){
+      return()
+    }
+    dt <- reactive.get.results.gender()$f
+    DT::formatRound(
+      DT::datatable( get.table(dt) ), 
+      columns = c("U5MR median", "IMR median"), digits = 2)
+  })
+  
+  
+  # Make results available for download
+   down.load.dt <- function(dt){
+    downloadHandler(
+      filename <- function() {
+        paste0("Results ", Sys.Date(), ".csv")
+      },
+      content <- function(file) {
+        write.csv(dt, file, row.names = F, na = "")
+    }
+  )}
+
+  output$download_table <- down.load.dt(dt = isolate(reactive.get.results()))
+  output$download_table_m <- down.load.dt(dt = isolate(reactive.get.results.gender())$m)
+  output$download_table_f <- down.load.dt(dt = isolate(reactive.get.results.gender())$f)
 }
 # Run App ---------------------------------------------------------------------
 shinyApp(ui, server)
